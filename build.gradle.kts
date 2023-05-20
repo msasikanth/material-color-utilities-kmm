797ad98940ef756eaab1c026ae196ff5c97ee262
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-05-20 19:23:00 +0530

    Publish dependencies to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,40 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    kotlin("multiplatform") version "1.8.10"
-    id("maven-publish")
+  kotlin("multiplatform") version "1.8.10"
+  id("com.vanniktech.maven.publish") version "0.25.2"
 }
 
 group = "dev.sasikanth"
 version = "1.0.0-alpha01"
 
+mavenPublishing {
+  publishToMavenCentral(host = SonatypeHost.S01, automaticRelease = true)
+  signAllPublications()
+}
+
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 kotlin {
-    jvm()
+  jvm()
 
-    ios()
-    iosSimulatorArm64()
+  ios()
+  iosSimulatorArm64()
 
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting
+  sourceSets {
+    val commonMain by getting
+    val commonTest by getting
 
-        val jvmMain by getting {
-            dependsOn(commonMain)
-        }
-
-        val iosSimulatorArm64Main by getting
-        val iosMain by getting {
-            dependsOn(commonMain)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
+    val jvmMain by getting {
+      dependsOn(commonMain)
     }
+
+    val iosSimulatorArm64Main by getting
+    val iosMain by getting {
+      dependsOn(commonMain)
+      iosSimulatorArm64Main.dependsOn(this)
+    }
+  }
 }
