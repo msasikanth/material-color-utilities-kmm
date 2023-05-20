cc2a52d675d26929c51c14b3ab6e42d6c9c4c554
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-05-20 16:29:53 +0530

    Convert `ToneDeltaConstraint` required params to constructor params

--- src/commonMain/kotlin/dev/sasikanth/material/color/utilities/dynamiccolor/ToneDeltaConstraint.kt
+++ src/commonMain/kotlin/dev/sasikanth/material/color/utilities/dynamiccolor/ToneDeltaConstraint.kt
@@ -21,19 +21,13 @@ import dev.sasikanth.material.color.utilities.dynamiccolor.TonePolarity
 /**
  * Documents a constraint between two DynamicColors, in which their tones must have a certain
  * distance from each other.
+ * @param delta the difference in tone required
+ * @param keepAway the color to distance in tone from
+ * @param keepAwayPolarity whether the color to keep away from must be lighter, darker, or no
+ * preference, in which case it should
  */
-class ToneDeltaConstraint(val delta: Double, keepAway: DynamicColor, keepAwayPolarity: TonePolarity) {
-  val keepAway: DynamicColor
+data class ToneDeltaConstraint(
+  val delta: Double,
+  val keepAway: DynamicColor,
   val keepAwayPolarity: TonePolarity
-
-  /**
-   * @param delta the difference in tone required
-   * @param keepAway the color to distance in tone from
-   * @param keepAwayPolarity whether the color to keep away from must be lighter, darker, or no
-   * preference, in which case it should
-   */
-  init {
-    this.keepAway = keepAway
-    this.keepAwayPolarity = keepAwayPolarity
-  }
-}
+  )
